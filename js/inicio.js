f13e7e24b7cabb10d4fd97c6f318b22e89d79167
TougheerW <134792671+TougheerW@users.noreply.github.com>
2024-09-21 23:21:42 -0500

    Add files via upload

---
+++ js/inicio.js
@@ -0,0 +1,7 @@
+onload = () =>{
+    document.body.classList.remove("container");
+};
+
+function mostrarMensaje() {
+    alert("Mañana tendrás otros 2 detalles. Espero te haya gustado esto, amor. Le he dedicado mucho tiempo, la verdad, después de muchos errores y de consultar a compañeros, salió esto, amor. ❤");
+    }
